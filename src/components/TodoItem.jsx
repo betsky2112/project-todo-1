625f64b2f637e37eed4ef0828de9d220202da855
betsky2112 <roberthussebastians@gmail.com>
2024-06-05 09:40:53 +0700

    Create Todos and TodoItem component

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const TodoItem = ({todo}) => {
+	return <p>{todo.title}</p>
+}
+
+export default TodoItem
